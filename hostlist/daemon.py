0d996aba431a9b5123e3a13ac31b09ab6d29ee26
Robin Roth <robin.roth@kit.edu>
2016-11-28 17:16:03 +0100

    Add caching and authentication

--- hostlist/daemon.py
+++ hostlist/daemon.py
@@ -4,8 +4,11 @@
 # from distutils.util import strtobool
 # import sys
 
-import cherrypy
 import git
+import logging
+import datetime
+import os.path
+import cherrypy
 
 from . import hostlist
 from . import cnamelist
@@ -16,17 +19,26 @@ from . import output_services
 class Inventory():
 
     def __init__(self):
-        self.repo = git.cmd.Git('.')
+        self.repo = git.Repo('.')
+        self.last_update = None
+        self.fetch_hostlist()
+        
+    def fetch_hostlist(self, timeout=600):
+        if self.last_update and datetime.datetime.now() - self.last_update < datetime.timedelta(seconds=timeout):
+            return
+        self.last_update = datetime.datetime.now()
 
-    def get_hostlist(self):
-        self.repo.pull()
+        pullresult = self.repo.remote().pull()[-1]
+        if not pullresult.flags & pullresult.HEAD_UPTODATE:
+            logging.error("Failed to pull hosts repo.")
         self.hosts = hostlist.YMLHostlist()
         self.cnames = cnamelist.FileCNamelist()
+        print("Refreshed cache.")
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def ansible(self):
-        self.get_hostlist()
+        self.fetch_hostlist()
         return output_services.AnsibleOutput.gen_content(
             self.hosts,
             self.cnames,
@@ -34,24 +46,43 @@ class Inventory():
 
     @cherrypy.expose
     def munin(self):
-        self.get_hostlist()
+        self.fetch_hostlist()
         return output_services.MuninOutput.gen_content(
             self.hosts,
             self.cnames,
         )
 
     @cherrypy.expose
+    def status(self):
+        result = 'Have a hostlist with %s hosts and %s cnames.' % (len(self.hosts), len(self.cnames))
+        result += '\nLast updated: %s' % self.last_update
+        return result
+
+    @cherrypy.expose
+    @cherrypy.config(**{'tools.caching.delay': 10})
+    def refreshcache(self):
+        cherrypy.lib.caching.cherrypy._cache.clear()
+        self.fetch_hostlist(timeout=10)
+
+    @cherrypy.expose
     def index(self):
-        self.get_hostlist()
         return 'See <a href="https://github.com/particleKIT/hostlist">github.com/particleKIT/hostlist</a> how to use this API.'
 
 
+def _auth_config(app):
+    if app.config.get('/', {}).get('tools.auth_basic.on', False):
+        auth = app.config['authentication']
+        def check_pass(realm, user, password):
+            return user == auth['user'] and password == auth['password']
+        app.config['/'].update({'tools.auth_basic.checkpassword': check_pass})
+
+
 def main():
-    cherrypy.config.update({
-        'server.socket_host': '0.0.0.0',
-        'server.socket_port': 80,
-    })
-    cherrypy.quickstart(Inventory())
+    app = cherrypy.tree.mount(Inventory(), '/', 'daemon.conf')
+    _auth_config(app)
+    cherrypy.engine.signals.subscribe()
+    cherrypy.engine.start()
+    cherrypy.engine.block()
 
 
 if __name__ == '__main__':
